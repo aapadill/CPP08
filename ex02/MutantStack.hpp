69591a38098e268500c2eabb321309e13b10cc93
Aaron Padilla <aapadill@student.hive.fi>
2026-02-20 12:05:27 +0200

    refactor to modern cpp, prev had too much bolierplate code

--- ex02/MutantStack.hpp
+++ ex02/MutantStack.hpp
@@ -12,31 +12,35 @@
 
 #pragma once
 
-# include <stack>
-# include <deque>
+#include <stack>
+#include <deque>
 
 template <typename T, typename Container = std::deque<T> >
 class MutantStack : public std::stack<T, Container>
 {
 	public:
-		typedef typename Container::iterator iterator;
-		typedef typename Container::const_iterator const_iterator;
-		typedef typename Container::reverse_iterator reverse_iterator;
-		typedef typename Container::const_reverse_iterator const_reverse_iterator;
+		//aliases
+		using iterator               = typename Container::iterator;
+		using const_iterator         = typename Container::const_iterator;
+		using reverse_iterator       = typename Container::reverse_iterator;
+		using const_reverse_iterator = typename Container::const_reverse_iterator;
 
-		MutantStack();
-		MutantStack(const MutantStack &other);
-		MutantStack &operator=(const MutantStack &other);
-		~MutantStack();
+		//ONF
+		//using std::stack<T, Container>::stack; //alternative
+		MutantStack() = default;
+		MutantStack(const MutantStack&) = default;
+		MutantStack &operator=(const MutantStack&) = default;
+		~MutantStack() = default;
 
-		iterator begin();
-		iterator end();
-		reverse_iterator rbegin();
-		reverse_iterator rend();
-		const_iterator begin() const;
-		const_iterator end() const;
-		const_reverse_iterator rbegin() const;
-		const_reverse_iterator rend() const;
+		iterator begin() { return this->c.begin(); }
+		iterator end() { return this->c.end(); }
+
+		const_iterator begin() const { return this->c.begin(); }
+		const_iterator end() const { return this->c.end(); }
+
+		reverse_iterator rbegin() { return this->c.rbegin(); }
+		reverse_iterator rend() { return this->c.rend(); }
+
+		const_reverse_iterator rbegin() const { return this->c.rbegin(); }
+		const_reverse_iterator rend() const { return this->c.rend(); }
 };
-
-# include "MutantStack.tpp"
--- ex02/main.cpp
+++ ex02/main.cpp
@@ -18,23 +18,32 @@ int main()
 	MutantStack<int> mstack;
 	mstack.push(5);
 	mstack.push(17);
-	std::cout << mstack.top() << std::endl;
-	mstack.pop();
-	std::cout << mstack.size() << std::endl;
+
+	std::cout << mstack.top() << std::endl; //prints 17
+	mstack.pop(); //removes 17
+	std::cout << mstack.size() << std::endl; //one
+
 	mstack.push(3);
 	mstack.push(5);
 	mstack.push(737);
 	mstack.push(0);
+	/*
+	-- current stack be like --
+		0  <---- end, top
+		737
+		5
+		3
+		5  <---- begin
+	*/
 	MutantStack<int>::iterator it = mstack.begin();
 	MutantStack<int>::iterator ite = mstack.end();
-	++it;
-	--it;
+	++it; //pos of 3
+	--it; //pos of 5
 	while (it != ite)
 	{
-	std::cout << *it << std::endl;
-	++it;
+		std::cout << *it << std::endl;
+		++it;
 	}
 	std::stack<int> s(mstack);
-	return 0;
 	return 0;
 }
